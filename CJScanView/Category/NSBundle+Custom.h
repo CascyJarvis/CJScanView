723c467b7785b83ce45f277cc080ebfc51dfe9da
CascyJarvis <jarvis.gjj@gmail.com>
2015-06-23 11:56:34 +0800

    将ScanView分离为单独文件夹

---
+++ CJScanView/Category/NSBundle+Custom.h
@@ -0,0 +1,17 @@
+//
+//  NSBundle+Custom.h
+//  AQPresentation
+//
+//  Created by Jarvis on 15/6/8.
+//  Copyright (c) 2015年 Jarvis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kBundleName @"CJResource"
+
+@interface NSBundle (Custom)
+
++ (NSBundle*) CustomBundle;
+
+@end
---
+++ CJScanViewDemo/CJScanViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJScanViewDemo
+//
+//  Created by Jarvis on 15/6/19.
+//  Copyright (c) 2015年 Jarvis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
